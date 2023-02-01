e62e1c21d62c4a486f8719928a46cdf39db974a9
andreportol <andreportol@gmail.com>
2023-02-01 16:54:43 -0400

    Sistema crud com class based views finalizado.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
 
-from .views import IndexView, createProdutoView
+from .views import (CreateProdutoView, DeleteProdutoView, IndexView,
+                    UpDateProdutoView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('add/', createProdutoView.as_view(), name='add/'),
+    path('add/', CreateProdutoView.as_view(), name='add/'),
+    path('<int:pk>/update/', UpDateProdutoView.as_view(), name='upd_produto'),
+    path('<int:pk>/delete/', DeleteProdutoView.as_view(), name='del_produto'),
 ]
--- core/views.py
+++ core/views.py
@@ -14,9 +14,19 @@ class IndexView(ListView):
     print(queryset)
     context_object_name = 'produtos' # nome da lista 'produtos'.
 
-class createProdutoView(CreateView):
+class CreateProdutoView(CreateView):
     model = Produto
     template_name = 'produto_form.html'
     fields = ['nome', 'preco']
     success_url = reverse_lazy('index')
 
+class UpDateProdutoView(UpdateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+class DeleteProdutoView(DeleteView):
+    model = Produto
+    template_name = 'produto_del.html'
+    success_url = reverse_lazy('index')
